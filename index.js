74f059adddd85df4eb084c41599b7a818511016f
Austin Kim <ask199416@gmail.com>
2017-10-10 02:14:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function destructivelyAppendKitten(name) {
   kittens === kittens.push(name)
   return kittens
 }
+
+function destructivelyPrependKitten(name) {
+  kittens === kittens.unshift(name)
+  return kittens
+}
